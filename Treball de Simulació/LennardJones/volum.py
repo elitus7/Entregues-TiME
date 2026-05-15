d19887cc8c6f2052beae5ca65021d1b9495d8916
juunshaan <juunshaan@gmail.com>
2026-05-15 20:55:22 +0200

    Update volum.py

---
+++ Treball de Simulació/LennardJones/volum.py
@@ -0,0 +1,33 @@
+import matplotlib.pyplot as plt
+
+# 1. Definició de les dades
+x = [0.9, 1, 1.1, 1.2, 1.25, 1.275]
+
+# Valors de dalt (variables)
+y_variable = [90.0847414, 83.4623758, 82.5201305, 82.3083279, 81.5869273, 81.5735469]
+
+# Valors de baix (constants)
+y_constant = [81.0930216] * len(x)
+
+# 2. Creació de la gràfica
+plt.figure(figsize=(10, 6))
+
+# Pintem els punts blaus (variables) i els taronja (constants)
+plt.scatter(x, y_variable, color='#1f77b4', label='Variable')
+plt.scatter(x, y_constant, color='#ff7f0e', label='Constant')
+
+# 3. Configuració d'eixos i títols
+plt.xlabel('$T$ normalitzada')
+plt.ylabel('$k_T$ normalitzada')
+plt.grid(True, linestyle='-', alpha=0.3)
+
+# Ajustem els límits perquè es vegi semblant a la teva imatge
+plt.ylim(80, 92)
+plt.xlim(0.85, 1.3)
+
+# 4. Desa el gràfic (això sobrescriurà el fitxer anterior cada cop que s'executi)
+plt.savefig('kt_volum.png')
+
+# Opcional: mostrar-lo per pantalla
+print("Gràfic 'kt_volum.png' generat correctament!")
+plt.show()
